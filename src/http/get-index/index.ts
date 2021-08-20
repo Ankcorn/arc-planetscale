5dee23f978539ade68366bddcee6cf7777797f7d
Ankcorn <thomasankcorn@gmail.com>
2021-08-20 18:00:53 +0100

    prisma targets

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -3,30 +3,36 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
 async function main() {
-  const posts = await prisma.post.findMany({
-    include: {
-      author: true
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: true
+      }
+    });
+  
+    return {
+      html: `<html>
+      <body>
+        <h1>Arc Planetscale Forum</h1>
+        <div>${posts.map(el => (`<li>${el?.author?.name} - ${el.title} - ${el.content || 'no content'} </li>`)).toString()
+        }</div>
+        <div>
+          <form method="post" action="comment">
+          <div><label>Title<input type="text" name="title" /></label</div>
+            
+            <div><label>Comment<textarea name="comment"></textarea></label></div>
+            <div><label>Email<input type="email" name="email" /></label></div>
+            <div><label>Name<input type="text" name="name" /></label></div>
+            <div><button type="submit">Submit</button> </div>
+          </form>
+        </div>
+      </body>
+      </html>`
     }
-  });
-
-  return {
-    html: `<html>
-    <body>
-      <h1>Arc Planetscale Forum</h1>
-      <div>${posts.map(el => (`<li>${el?.author?.name} - ${el.title} - ${el.content || 'no content'} </li>`)).toString()
-      }</div>
-      <div>
-        <form method="post" action="comment">
-        <div><label>Title<input type="text" name="title" /></label</div>
-          
-          <div><label>Comment<textarea name="comment"></textarea></label></div>
-          <div><label>Email<input type="email" name="email" /></label></div>
-          <div><label>Name<input type="text" name="name" /></label></div>
-          <div><button type="submit">Submit</button> </div>
-        </form>
-      </div>
-    </body>
-    </html>`
+  } catch(e) {
+    return {
+      html: e.message
+    }
   }
 }
 
--- src/http/post-comment/index.ts
+++ src/http/post-comment/index.ts
@@ -3,33 +3,39 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
 async function main(req: HttpRequest) {
-	const input: { name: string, title: string, comment: string, email: string } = req.body
+	try {
+		const input: { name: string, title: string, comment: string, email: string } = req.body
 
-	let user = await prisma.user.findUnique({
-		where: {
-			email: input.email
-		}
-	})
-
-	if (!user) {
-		user = await prisma.user.create({
-			data: {
-				name: input.name,
+		let user = await prisma.user.findUnique({
+			where: {
 				email: input.email
 			}
-		});
-	}
-	await prisma.post.create({
-		data: {
-			title: input.title,
-			content: input.comment,
-			authorId: user.id
+		})
+
+		if (!user) {
+			user = await prisma.user.create({
+				data: {
+					name: input.name,
+					email: input.email
+				}
+			});
 		}
-	})
-	return {
-		status: 302,
-		headers: {
-			location: '/'
+		await prisma.post.create({
+			data: {
+				title: input.title,
+				content: input.comment,
+				authorId: user.id
+			}
+		})
+		return {
+			status: 302,
+			headers: {
+				location: '/'
+			}
+		}
+	} catch (e) {
+		return {
+			html: e.message
 		}
 	}
 }
